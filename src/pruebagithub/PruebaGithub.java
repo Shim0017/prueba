2cb39fecc81c4be52048c993fe20066f63f44b79
cloweling <redanimedark@gmail.com>
2018-02-17 13:41:33 -0600

    Agregado cambios

--- src/pruebagithub/PruebaGithub.java
+++ src/pruebagithub/PruebaGithub.java
@@ -5,6 +5,8 @@
  */
 package pruebagithub;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author cloweling
@@ -19,7 +21,20 @@ public class PruebaGithub {
     /**
      * @param args the command line arguments
      */
+    
+    public static void ballenitatumama(){
+        int prueba = 5;
+        
+        for( int i = 0; i <= prueba; i++){
+            if(prueba != 2){
+                 JOptionPane.showMessageDialog(null, "TU MAMA ES BALLENITA");
+            }else{
+                JOptionPane.showMessageDialog(null, "TU MAMA ES LA SAMI");
+            }
+        }
+    }
     public static void main(String[] args) {
+        
         // TODO code application logic here
         System.out.println(""+suma(1,2));
     }
